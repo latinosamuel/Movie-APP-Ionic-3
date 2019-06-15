d4e6fa2495062f2c53fd1269238253b1e8abb977
Samuel Latino <samuel.latino@xpand-it.com>
2019-06-15 19:58:55 +0100

    adding ngFor directive to an array of string

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   public welcome : string = "Welcome to Ionic !";
+  public int : number = 0;
+  public bool : Boolean = true;
+  public list : Array<string> = ['Rita','João','Carla','Mariana','Carlos'];
+  public listAny : Array<any> = ['Teste',10,true];
 
   constructor(public navCtrl: NavController) {
 
