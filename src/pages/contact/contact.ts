75e2f2a98fad2de7dd1dbb65a5d84d3cd07396e7
Samuel Latino <samuel.latino@xpand-it.com>
2019-06-16 10:26:03 +0100

    adding * NgFor with Array of Objects

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,8 +7,25 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  public contacts : Array<Object> = [];
   constructor(public navCtrl: NavController) {
-
+      this.contacts = [
+        {
+          "nome": "Woody",
+          "img" : "https://ionicframework.com/docs/v3/dist/preview-app/www/assets/img/avatar-ts-woody.png",
+          "msg" : "This town ain't big enough for the two of us!"
+        },
+        {
+          "nome": "Buzz Lightyear",
+          "img" : "https://ionicframework.com/docs/v3/dist/preview-app/www/assets/img/avatar-ts-buzz.png",
+          "msg" : "My eyeballs could have been sucked from their sockets!"
+        },
+        { 
+          "nome": "Jessie",
+          "img" : "https://ionicframework.com/docs/v3/dist/preview-app/www/assets/img/avatar-ts-jessie.png",
+          "msg" : "Well aren't you just the sweetest space toy I ever did meet!"
+        }
+      ]
   }
 
 }
